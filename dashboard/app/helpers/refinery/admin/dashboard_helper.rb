f655947a0a19d230aa2bcceefc333a45082ea757
Matt Clark <winescout@gmail.com>
2011-12-16 00:23:17 -0800

    handle bad url generation somewhat gracefully

--- dashboard/app/helpers/refinery/admin/dashboard_helper.rb
+++ dashboard/app/helpers/refinery/admin/dashboard_helper.rb
@@ -11,12 +11,18 @@ module ::Refinery
           # get article to define gender of model name, some languages require this for proper grammar
           article = t('article', :scope => "refinery.plugins.#{plugin.name}.", :default => 'the')
 
-          # now create a link to the notification's corresponding record.
-          link_to t('.latest_activity_message',
-                    :what => record.send(activity.title),
-                    :kind => record.class.model_name.human,
-                    :action => t("with_article \"#{article}\"", :scope => "refinery.#{action}")
-                   ).downcase.capitalize, eval("main_app.#{activity.url}(#{activity.nesting("record")}record)")
+          begin
+            # now create a link to the notification's corresponding record.
+            generated_route = eval("main_app.#{activity.url}(#{activity.nesting("record")}record)")
+            link_to t('.latest_activity_message',
+                      :what => record.send(activity.title),
+                      :kind => record.class.model_name.human,
+                      :action => t("with_article \"#{article}\"", :scope => "refinery.#{action}")
+                     ).downcase.capitalize, generated_route          
+          rescue
+            logger.debug "failed to generate route for #{generated_route}"
+            return nil
+          end
         end
       end
 
